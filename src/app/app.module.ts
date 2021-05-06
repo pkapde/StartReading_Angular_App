971c91392ceed559a7eaa58ff1206f4086463106
Prashant Kapade <Prashant.Kapade@cuelogic.local>
2021-05-06 12:10:48 +0530

    Added:code to display available books to read

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './Component/nav-bar/nav-bar.component';
+import { IntroComponent } from './Component/intro/intro.component';
+import { BooksComponent } from './Component/books/books.component';
+import { FooterComponent } from './Component/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    IntroComponent,
+    BooksComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
